78b819e8c6a85af2c5ec259d1e779ba5f7c7b360
Mohd Shaheer <mshaheer2003@gmail.com>
2025-09-22 19:13:47 +0530

    added additional models & setup uploadthing for image upload

---
+++ tryon/src/app/components/upload.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { UploadButton } from "../utils/uploadthing";
+
+export default function UploadBtn(){
+    return (
+        <>
+        <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+        </>
+    );
+}
--- tryon/src/app/page.tsx
+++ tryon/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import SignIn from "./components/sign-in";
 import SignOut from "./components/sign-out";
 import { auth } from "../../auth";
+import UploadBtn from "./components/upload";
 
 export default async function Home() {
   const session = await auth()
@@ -11,6 +12,7 @@ export default async function Home() {
       <h3> Hi {session?.user?.name}</h3>
       {session && <Image src={session?.user?.image || ""} alt="User Image" width={100} height={100} />}
       {session ?  <SignOut /> : <SignIn />}
+      <UploadBtn/>
     </div>
   );
 }
